c38dd770c2bae69b350b052fa6f11633a2204804
Donniecia  Corke <donnieciamc@gmail.com>
2022-11-12 11:58:18 -0500

    removing files out of develop folder installing packages

---
+++ server/server.js
@@ -0,0 +1,26 @@
+//importing the dependencies
+const express = require('express');
+const { AppolloServer } = require('appollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+//importing the typeDefs and resolvers
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
