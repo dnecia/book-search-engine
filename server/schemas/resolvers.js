246724fb90304c9559e9a8e5145106c7a6625fb0
Donniecia  Corke <donnieciamc@gmail.com>
2022-11-12 23:10:20 -0500

    getting the saved books link to work! finall

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,13 +5,14 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
-            if (context.user) {
-                data = await (await User.findOne({ _id: context.user._id })).select('-__v -password');
-                return data;
-            }
-            throw new AuthenticationError('Please Log In');
-        },
-    },
+                    if (context.user) {
+                        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+                
+                        return userData;
+                      }
+                    throw new AuthenticationError('Please Log In');
+                },
+            },
 
     Mutation: {
         addUser: async (parent, { username, email, password }) => {
